5a50c0c5a1d0cf4179b3c3ffcac54143e3185b3d
Zezinhotwo <107373132+Zezinhotwo@users.noreply.github.com>
2026-01-22 00:41:07 -0300

    DepartmentDaoJDBC FindAll Implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -19,13 +19,21 @@ public class Program2 {
 		dep.setName("food");
 		department.update(dep);
 		System.out.println("Update Success");
+		
+		// FIND_ALL
+		department.findAll().forEach(System.out::println);
+		// END_FIND_ALL
 
 		System.out.println("==== TEST 3: Delete ====");
-//		department.deletById(dep.getId());
+		department.deletById(dep.getId());
 		System.out.println("Delete Success");
 
 		System.out.println("==== TEST 4: FindById ====");
 		System.out.println("FindById Success: " + department.findById(1));
+
+		System.out.println("==== TEST 5: findAll ====");
+		department.findAll().forEach(System.out::println);
+		System.out.println("findAll Success");
 
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -95,8 +96,23 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement("SELECT * FROM department ORDER BY Name ");
+
+			rs = st.executeQuery();
+			List<Department> list = new ArrayList<>();
+
+			while (rs.next()) {
+				list.add(new Department(rs.getInt("Id"), rs.getString("Name")));
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+
 	}
 
 	@Override
